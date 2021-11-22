cf9c48b1e4f749c657478163ca7b5b262fde3579
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-11-22 11:38:59 +0900

    [UPDATE] Allowed hosts all

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l80aw+3ec7jl5$t$^trol94y3t_%z5@b+^fuh+s&#k^0gg!d-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
