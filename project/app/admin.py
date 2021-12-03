19cfcc607645815f1d7cf9078f859e8519c1162d
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-12-03 15:52:40 +0900

    [UPDATE] regist models in admin

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Founder)
+admin.site.register(FactoryOwner)
+admin.site.register(ContactUsers)
+admin.site.register(FounderEstimate)
+admin.site.register(FactoryInformation)
+admin.site.register(KeywordList)
