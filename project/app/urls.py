1638a411bda587833245c862aabc2580da8c2e0c
DESKTOP-4MNBB96\sdat7 <sdat789@naver.com>
2021-12-05 00:36:02 +0900

    fix sever error...1 fix url address

--- project/app/urls.py
+++ project/app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('recommend/<str:user_id>', RecommendView.as_view()),
     path('founder/', FounderView.as_view()),
     path('factory/', FactoryView.as_view()),
-    path('founder/estimate/', FounderEstView.as_view()),
-    path('factory/information/', FactoryInfoView.as_view()),
+    path('founder_estimate/', FounderEstView.as_view()),
+    path('factory_information/', FactoryInfoView.as_view()),
 ]
